cf93ba5dcecad27d331853304d4f6d58303fe0f3
istiak227 <istiak.ahmed227@gmail.com>
2021-02-13 19:55:03 +0600

    testing clock on  APP

--- src/App.js
+++ src/App.js
@@ -8,12 +8,28 @@ import Footer from './Components/Footer';
 
 
 function App() {
+  const date = new Date()
+  const hour = date.getHours()
+  let timeOfDay
+
+  if (hour<12) {
+    timeOfDay = "mornign"
+  } else if (hour >= 12 && hour < 17) {
+    timeOfDay = "afternoon"
+  } else {
+    timeOfDay = "night"
+  }
+
   return (
     <div className="App">
   
      <Header />
      <MyInfo />
      <Footer />
+
+     <h3>
+       Good {timeOfDay}!
+     </h3>
 
 
     </div>
--- src/Components/MyInfo.js
+++ src/Components/MyInfo.js
@@ -1,26 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './MyInfo.css';
 
 const MyInfo = () => {
-    const style = {
-        padding: "10px",
-        margin: "10px",
-        backgroundColor: "DodgerBlue"
-    }
-    const listStle = {
-        listStyleType: "none",
-        justifyContent: "Space-between",
-        margin: "5px"
-    }
     return (
         <main>
-            <h1 style={style}>My Name is Istiak</h1>
+            <h1 className="title">My Name is Istiak</h1>
             <p style={{padding: "10px", margin: "5px"}}>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos nulla, similique rem voluptatum unde nisi iure? Voluptatibus aliquid voluptate iusto dolorum perspiciatis eligendi ex dolor quos! Est doloremque voluptatum natus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos nulla, similique rem voluptatum unde nisi iure? Voluptatibus aliquid voluptate iusto dolorum perspiciatis eligendi ex dolor quos! Est doloremque voluptatum natus! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos nulla, similique rem voluptatum unde nisi iure? Voluptatibus aliquid voluptate iusto dolorum perspiciatis eligendi ex dolor quos! Est doloremque voluptatum natus!
             </p>
             <ul>
-                <li style={listStle}>Cox</li>
-                <li style={listStle}>Malaysia</li>
-                <li style={listStle}>Sylhet</li>
+                <li className="list">Cox</li>
+                <li className="list">Malaysia</li>
+                <li className="list">Sylhet</li>
             </ul>
         </main>
     )
